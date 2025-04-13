4d8501672c89427c0692512b31447653263c892d
Phyo Hein ( Ezekiel ) <mwrcps11@gmail.com>
2025-04-13 23:55:30 +0700

    Finished Logic Test 1

--- app/src/main/java/com/talentprogramming/logictest1/MainActivity.kt
+++ app/src/main/java/com/talentprogramming/logictest1/MainActivity.kt
@@ -12,9 +12,10 @@ import com.talentprogramming.logictest1.databinding.ActivityMainBinding
 @Suppress("CAST_NEVER_SUCCEEDS", "IMPLICIT_CAST_TO_ANY")
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private var firstValue : Double? = null
+    private var firstValueText : String = ""
+    private var result : Number? = null
+    private var lastOperator: String? = null
     private var operator : String? = null
-    private var isNewCalculation : Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -23,11 +24,13 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onNumberClicked(view: View) {
-        isNewCalculation = renewCalculation()
+        if(lastOperator == "="){
+            clearCalculation()
+        }
         val btnNumber = view as Button
         val etCalculate = binding.etCalculate.text.toString()
         if(etCalculate == "0" || etCalculate == "00"){
-            binding.etCalculate.setText("0")
+            binding.etCalculate.text = "0"
         }
         else{
             binding.etCalculate.append(btnNumber.text.toString())
@@ -36,67 +39,99 @@ class MainActivity : AppCompatActivity() {
     fun onOperatorClicked(view: View) {
         val btnOperator = view as Button
         operator = btnOperator.text.toString()
-        val value = binding.etCalculate.text.toString()
-        if(value.equals("")){
+        firstValueText = binding.etCalculate.text.toString()
+        if(firstValueText == ""){
             Toast.makeText(this@MainActivity, "Enter a number first", Toast.LENGTH_SHORT).show()
         }
-        firstValue = value.toDouble()
-        binding.tvOperation.text = firstValue.toString()
-        binding.tvOperator.text = operator
-        binding.etCalculate.text = ""
-    }
-    @SuppressLint("SetTextI18n")
-    fun onEqualClicked(view: View) {
-        val secondValueText = binding.etCalculate.text.toString()
-        try {
-            if (secondValueText.equals("")) {
-                Toast.makeText(this@MainActivity, "Please select a second number", Toast.LENGTH_SHORT)
-                    .show()
-            }
-            val secondValue = secondValueText.toDouble()
-            val result = when (operator) {
-                "+" -> firstValue!! + secondValue
-                "-" -> firstValue!! - secondValue
-                "×" -> firstValue!! * secondValue
-                "%" -> firstValue!! % secondValue
-                "÷" -> if (secondValue != 0.0) firstValue!! / secondValue else "Cannot Divided By 0"
-                else -> {
-                    Toast.makeText(this@MainActivity, "Calculation Error 5", Toast.LENGTH_SHORT)
-                        .show()
-                }
-            }
-            binding.tvOperation.text = "$firstValue $operator $secondValue"
-            binding.etCalculate.text = result.toString()
-            binding.tvOperator.text = "="
-            isNewCalculation = true
-        }catch (e : Exception){
-            Log.d("Error", e.message.toString())
+        else{
+            binding.tvOperation.text = firstValueText
+            binding.tvOperator.text = operator
+            binding.etCalculate.text = ""
         }
     }
+    @SuppressLint("SetTextI18n")
+    fun onEqualClicked(view : View) {
+        Log.d("latest Operator", lastOperator.toString())
+        val secondValueText = binding.etCalculate.text.toString()
+        lastOperator = binding.tvOperator.text.toString()
+        if (secondValueText == "") {
+            Toast.makeText(this@MainActivity, "Please enter a second number", Toast.LENGTH_SHORT)
+                .show()
+        }
+        else if(firstValueText == "" && secondValueText == ""){
+            Toast.makeText(this@MainActivity, "Please enter a number first", Toast.LENGTH_SHORT)
+                .show()
+        }
+        else if(lastOperator != "="  && binding.etCalculate.text.toString() != result.toString()){
+            try{
+                val firstValue = firstValueText.toDouble()
+                val secondValue = secondValueText.toDouble()
+                result = when (operator) {
+                    "+" -> firstValue + secondValue
+                    "-" -> firstValue - secondValue
+                    "×" -> firstValue * secondValue
+                    "÷" -> ((if(secondValue != 0.0 ){
+                        firstValue / secondValue
+                    } else{
+                        Toast.makeText(this@MainActivity, "Cannot divided by 0", Toast.LENGTH_LONG).show()
+                    }) as Number)
+                    else -> {
+                        Toast.makeText(this@MainActivity, "Calculation Error", Toast.LENGTH_SHORT)
+                            .show() as Number
+                    }
+                }
+                binding.tvOperation.text = "$firstValueText $operator $secondValueText"
+                binding.etCalculate.text = (if(result!!.toDouble() % 1.0 == 0.0)  result!!.toInt() else result).toString()
+                binding.tvOperator.text = "="
+            }
+            catch (e : Exception){
+                Log.d("Error", e.message.toString())
+            }
+        }
+
+    }
     fun onClearClicked(view: View) {
+        firstValueText = ""
         binding.etCalculate.text = ""
-        binding.tvOperation.text = null
-        binding.tvOperator.text = null
-        isNewCalculation = false
+        binding.tvOperation.text = ""
+        binding.tvOperator.text = ""
+        result = null
+    }
+    private fun clearCalculation(){
+        Log.d("Latest Operation", lastOperator.toString())
+        firstValueText = ""
+        binding.etCalculate.text = ""
+        binding.tvOperation.text = ""
+        binding.tvOperator.text = ""
+        result = null
+        lastOperator = null
 
     }
     fun onBackSpaceClicked(view: View) {
         val etValue = binding.etCalculate.text.toString()
-        if(!etValue.equals("")){
+        if(etValue != ""){
             val etNewValue = etValue.substring(0, etValue.length - 1)
             binding.etCalculate.text = etNewValue
         }
+        else if(lastOperator == "=") {
+            clearCalculation()
+        }
         else{
-            binding.etCalculate.setText("0")
+            binding.etCalculate.text = "0"
         }
 
     }
 
-    private fun renewCalculation() : Boolean{
-        if(isNewCalculation){
-            onClearClicked(binding.btClear)
+    fun onPercentageClicked(view: View) {}
+    fun onDecimalClicked(view: View) {
+        val currentValue = binding.etCalculate.text.toString()
+        if(!currentValue.contains(".")){
+            if(currentValue.isEmpty()){
+                binding.etCalculate.text = "0."
+            }
+            else{
+                binding.etCalculate.append(".")
+            }
         }
-        return false
     }
-
 }
